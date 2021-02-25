e8779404f2360f7ba3a84671b4371e781273c21a
Milagros <mruiz.oporta@gmail.com>
2021-02-25 15:40:21 -0600

    Google signin

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,10 @@
 const { response } = require('express');
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 const bcryptjs = require('bcryptjs');
 const Usuario = require('../models/usuario');
+
 
 const login = async(req, res = response) => {
 
@@ -48,11 +50,55 @@ const login = async(req, res = response) => {
         })
 
     }
+}
 
+const googleSignin = async(req, res = response) => {
+    const { id_token } = req.body;
 
+    try {
+        const { correo, nombre, img } = await googleVerify(id_token);
 
+        //Verificar si correo existe en la base de datos
+        let usuario = await Usuario.findOne({ correo });
+
+        if (!usuario) {
+            //Tengo que crearlo
+            const data = {
+                nombre,
+                correo,
+                password: ':p',
+                img,
+                google: true
+            };
+
+            usuario = new Usuario(data);
+            await usuario.save();
+        }
+
+        //Si el usuario en base de datos es falso
+        if (!usuario.estado) {
+            return res.status(401).json({
+                msg: 'Hable con el administrador. Usuario Bloqueado'
+            });
+        }
+
+        //generar el JWT
+        const token = await generarJWT(usuario.id);
+
+        res.json({
+            usuario,
+            token
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({
+            msg: 'Token de Google no es valido'
+        })
+    }
 }
 
 module.exports = {
-    login
+    login,
+    googleSignin
 }
